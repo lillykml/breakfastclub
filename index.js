a675bdf3e2a21a99917f2327bf8a79a63d691d4d
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-12 12:19:46 +0200

    Prepared for static frontend connection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 const { generateId } = require('./utils/helpers');
+app.use(express.static('dist'))
 
 
 let brunches = [
@@ -87,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
