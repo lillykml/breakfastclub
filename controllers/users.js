23a507051441171711202ebc90cb8d5981fa0b0a
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-17 15:15:53 +0200

    Fixed missing password error

--- controllers/users.js
+++ controllers/users.js
@@ -12,15 +12,19 @@ usersRouter.get('/', async (request, response) => {
 
 
 usersRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  const body = request.body
+  
+  if (!body.password) {
+    return response.status(400).json({ error: 'password required' });
+  }
 
   const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
   const user = new User({
-    username,
-    name,
-    passwordHash,
+    username: body.username,
+    name: body.name,
+    passwordHash: passwordHash
   })
 
   const savedUser = await user.save()
